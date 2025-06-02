fd29b7740d04674f3d10430ef285e5f97708c64c
Franco <francob1997@gmail.com>
2025-06-02 09:58:53 +0000

    Segundo commit con el programa de calculos de promedios

--- tpintegrador.py
+++ tpintegrador.py
@@ -1,1 +1,9 @@
-print('Hola hola')
+suma=0
+
+for i in range(3):
+    numero=float(input("Ingrese un numero: "))
+    suma+= numero
+
+prom= suma/3
+
+print(f"El promedio de los 3 numeros es: {prom}")
